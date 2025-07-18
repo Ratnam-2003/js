75560dea50c6e434ea2d30db3bceab3e6dd8b955
chinni <nookaratnamtamanari@gmail.com>
2025-07-18 23:56:23 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,80 @@
+
+console.log("hello world")
+        const a=10
+        const b=20
+        console.log(a+b)
+//1. Identifires:
+
+//a. Identifires allows alphabets
+const c=30
+console.log(c)
+
+//b. identifiress allows case sensitive
+const d=40
+console.log(d)
+
+const D=50
+console.log(D)
+
+//c.identifires allows (_)
+const _e=60
+console.log(_e)
+
+/* 2.Variables */
+
+//a.var
+ var namee="chinni"
+ var  namee="ratnam"
+ console.log(namee)
+
+ //b.let
+let mom="stya"
+console.log(mom)
+
+//c.const
+const app="js"
+console.log(app)
+
+/* 3.Hoisting --> only possible with var*/
+console.log(name)
+var name="salt"
+
+/*4.Datatypes*/
+
+//A.number datatype
+
+const number=33.8
+console.log(number)
+console.log(typeof number)
+/*
+ const number=NaN
+console.log(number)
+console.log(typeof number)
+ */
+
+//B.boolean
+const num=true
+console.log(num)
+console.log(typeof num)
+
+const boolean=false
+console.log(boolean)
+console.log(typeof boolean)
+
+//c.String
+const numbe="String"
+console.log(numbe)
+console.log(typeof numbe)
+
+//D.object
+//i.array[]
+const numberr=[10,20,30,40]
+console.log(numberr)
+console.log(typeof numberr)
+
+//ii.object{}
+const object={"name":"chinni",age:21}
+console.log(object)
+console.log(typeof object)
+
+
